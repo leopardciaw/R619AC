41e8145d67a57c5cefde1a6c5d0adb7b96a936a5
leopardciaw <quanyongw@gmail.com>
2020-03-09 20:03:43 +0800

    update

--- diy.sh
+++ diy.sh
@@ -9,8 +9,14 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 
 # 删除原主题包
-rm -rf package/lean/luci-theme-argon
+# rm -rf package/lean/luci-theme-argon
 # rm -rf openwrt/package/lean/luci-theme-netgear
+
+# Modify the version number
+sed -i 's/OpenWrt/Leopard build $(date "+%Y.%m.%d") @ OpenWrt/g' package/lean/default-settings/files/zzz-default-settings
+
+# Modify default theme
+# sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 添加新的主题包
 # git clone https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
@@ -23,3 +29,14 @@ git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/lean/lu
 
 #增加koolddns
 git clone https://github.com/xrouterservice/luci-app-koolddns.git openwrt/package/lean/luci-app-koolddns
+
+# Add kernel build user
+[ -z $(grep "CONFIG_KERNEL_BUILD_USER=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_USER="Leopard"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_USER=\).*@\1$"P3TERX"@' .config
+
+# Add kernel build domain
+[ -z $(grep "CONFIG_KERNEL_BUILD_DOMAIN=" .config) ] &&
+    echo 'CONFIG_KERNEL_BUILD_DOMAIN="GitHub Actions"' >>.config ||
+    sed -i 's@\(CONFIG_KERNEL_BUILD_DOMAIN=\).*@\1$"GitHub Actions"@' .config
+
