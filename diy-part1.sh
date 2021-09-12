150b542608c8a98ac72cad5340228f529c683b84
leopardciaw <47556424+leopardciaw@users.noreply.github.com>
2021-09-12 22:35:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,7 @@
 #获取Lienol-xiaorouji-passwall
 # git clone https://github.com/xiaorouji/openwrt-package/lienol/ package/diy-packages/lienol
 # git clone https://github.com/xiaorouji/openwrt-package/tree/master/lienol/luci-app-passwall package/luci-app-passwall
-git clone https://github.com/kenzok8/openwrt-packages.git package/diy-packages
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
 git clone https://github.com/kenzok8/small.git package/small
 
 # 增加ssr（上述kenzok8/openwrt-packages已包含）
@@ -25,4 +25,4 @@ git clone https://github.com/kenzok8/small.git package/small
 # git clone https://github.com/fw876/helloworld.git package/diy-packages/luci-app-ssr-plus
 
 #  增加luci-app-unblockneteasemusic，来源immortalwrt
-git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/diy-packages
+git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
